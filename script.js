be5b27f6cc2015da2edf72511df0c47e8aed427c
devjunior30 <edyjunior299@gmail.com>
2023-06-26 23:37:41 -0300

    mexendo no script adicionando novas funções

---
+++ script.js
@@ -0,0 +1,34 @@
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector(".circulo")
+let fundo = document.querySelector("body")
+let paragrafo = document.querySelector("p")
+let input = document.querySelector("#color")
+
+function trocaImagem(endereco) {
+    imagem.src = endereco
+}
+
+function trocaCor(cor) {
+    circulo.style.background = cor
+}
+
+function trquei(parametro) {
+    fundo.style.backgroundColor = "	#808080"
+    paragrafo.style.color = 'gold'
+}
+
+function troquei() {
+    fundo.style.backgroundColor ="rgb(105,89,205)"
+    paragrafo.style.color = 'red'
+}
+
+function troquei2() {
+    fundo.style.backgroundColor ="#8B0000"
+    paragrafo.style.color = 'white'
+}
+
+function trocaTudo() {
+    circulo.style ='clip-path: circle(40%);'
+    circulo.style.right = "30%"
+    
+}
